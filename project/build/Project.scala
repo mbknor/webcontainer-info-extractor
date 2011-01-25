699990ef25ca24c4b70c9ef7512a0e141d8b5fd2
Morten Kjetland <mbk@kjetland.com>
2011-01-25 09:44:52 +0100

    ready to release

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -2,5 +2,20 @@ import sbt._
 
 class Project(info: ProjectInfo) extends DefaultProject(info) with IdeaProject
 {
-	val scalaSnapshotRepo = "scala snapshot" at "http://scala-tools.org/repo-snapshots/"
+	
+  override def managedStyle = ManagedStyle.Maven
+
+  val publishTo = Resolver.ssh("my local mbknor repo", "localhost", "~/projects/mbknor.github.com/m2repo/releases/")
+
+  Resolver.userMavenRoot
+
+  //include doc and source in publish
+  override def packageDocsJar = defaultJarPath("-javadoc.jar")
+  override def packageSrcJar= defaultJarPath("-sources.jar")
+  val sourceArtifact = Artifact.sources(artifactID)
+  val docsArtifact = Artifact.javadoc(artifactID)
+  override def packageToPublishActions = super.packageToPublishActions ++ Seq(packageDocs, packageSrc)
+  
+	
+  val scalaSnapshotRepo = "scala snapshot" at "http://scala-tools.org/repo-snapshots/"
 }
